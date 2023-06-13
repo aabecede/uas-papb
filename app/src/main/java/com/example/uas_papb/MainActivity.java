2f3129787c0daf0d7061a3916dc7420f1926f4d9
aabecede <aabecede>
2023-06-13 10:37:25 +0700

    FIRST INIT

---
+++ app/src/main/java/com/example/uas_papb/MainActivity.java
@@ -0,0 +1,89 @@
+package com.example.uas_papb;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    List<ModelTodoActivity> modelTodoActivities = new ArrayList<>();
+    String  url = "https://mgm.ub.ac.id/todo.php";
+    private RecyclerView rvTodo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        rvTodo = (RecyclerView) findViewById(R.id.rvTodo);
+        rvTodo.setLayoutManager(new LinearLayoutManager( this));
+        getData();
+    }
+
+    /**
+     * Digunakan untuk get data Api volley ke Main Activity
+     * @return
+     */
+    private void getData(){
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.e("API", "response : " + response);
+                try {
+                    JSONArray jsonArray = new JSONArray(response);
+
+                    // Iterate through each item in the JSON array
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        JSONObject item = jsonArray.getJSONObject(i);
+                        ModelTodoActivity modelTodo = new ModelTodoActivity(item.getString("time"), item.getString("what"));
+                        modelTodoActivities.add(modelTodo);
+                        /**
+                         * cek data
+                         */
+//                        int id = item.getInt("id");
+//                        String what = item.getString("what");
+//                        String time = item.getString("time");
+//
+//                        // Perform your desired operation on each item
+//                        System.out.println("ID: " + id);
+//                        System.out.println("What: " + what);
+//                        System.out.println("Time: " + time);
+//                        System.out.println();
+                    }
+                    rvTodo.setAdapter(new TodoAdapter(MainActivity.this.modelTodoActivities));
+
+                } catch (JSONException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("api", "onErrorResponse: " + error.getLocalizedMessage());
+            }
+        });
+
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
+    }
+}
---
+++ app/src/main/java/com/example/uas_papb/TodoAdapter.java
@@ -0,0 +1,52 @@
+package com.example.uas_papb;
+
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class TodoAdapter extends RecyclerView.Adapter<TodoAdapter.ViewHolder> {
+    private List<ModelTodoActivity> mTodoAdapter;
+
+    public TodoAdapter(List<ModelTodoActivity> todoList) {
+        mTodoAdapter = todoList;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        // inflate the layout for each item in the RecyclerView
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.todo_activity, parent, false);
+        ViewHolder viewHolder = new ViewHolder(view);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        // bind the data to the views in each item of the RecyclerView
+        ModelTodoActivity todoObject = mTodoAdapter.get(position);
+        holder.timeTextView.setText(todoObject.time);
+        holder.todoTextView.setText(todoObject.todo);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mTodoAdapter.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView timeTextView;
+        TextView todoTextView;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            timeTextView = (TextView) itemView.findViewById(R.id.time_text_view);
+            todoTextView = (TextView) itemView.findViewById(R.id.todo_text_view);
+            timeTextView.setTextColor(Color.BLACK);
+        }
+    }
+}
